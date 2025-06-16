fac74ea102e992ae3e2184d6d1746c827a3a8db8
tranarchy <tranarchy>
2025-06-16 23:44:33 +0200

    hide gpu_init on apple

--- include/main.h
+++ include/main.h
@@ -2,4 +2,7 @@ void gen_init(struct gen_info *gen);
 void cpu_init(struct cpu_info *cpu);
 void mem_init(struct mem_info *mem);
 void storage_init(struct storage_info *storage);
-void gpu_init(struct gpu_info *gpu_info);
+
+#ifndef __APPLE__
+    void gpu_init(struct gpu_info *gpu_info);
+#endif
